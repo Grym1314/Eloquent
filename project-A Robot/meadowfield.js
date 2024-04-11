87fcb4bd335b8da6e0f41d468612d80af1bccca3
Grym1314 <chucho_031199@hotmail.com>
2024-04-10 19:55:40 -0600

    #7 first array Meadowfield

--- project-A Robot/meadowfield.js
+++ project-A Robot/meadowfield.js
@@ -1,9 +1,9 @@
 const roads = [
-    "Casa de Alice-Casa de Bob","Casa de Alice-Cabaña",
-    "Casa de Alice-Oficina de Correos","Casa de Bob-Ayuntamiento",
-    "Casa de Daria-Casa de Ernie","Casa de Daria-Ayuntamiento",
-    "Casa de Ernie-Casa de Grete","Casa de Grete-Granja",
-    "Casa de Grete-Tienda","Plaza de Mercado-Granja",
+    "Casa de Leslie-Casa de Jesus","Casa de Leslie-Cabaña",
+    "Casa de Leslie-Oficina de Correos","Casa de Jesus-Ayuntamiento",
+    "Casa de Ariel-Casa de Javier","Casa de Ariel-Ayuntamiento",
+    "Casa de Javier-Casa de Danie;a","Casa de Danie;a-Granja",
+    "Casa de Danie;a-Tienda","Plaza de Mercado-Granja",
     "Plaza de Mercado-Oficina de Correos","Plaza de Mercado-Tienda",
     "Plaza de Mercado-Ayuntamiento","Tienda-Ayuntamiento"
   ];
